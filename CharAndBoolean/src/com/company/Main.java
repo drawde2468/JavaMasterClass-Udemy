8b54ff7c43e3243168bccc795e8ec886e5496908
Edward Leslie <edwardleslie09@gmail.com>
2019-04-09 11:03:08 -0400

    completed lesson 21 char and bool

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //width of 16 (2 bytes)
+        //can use single character or unicode
+        char myChar = '\u00A9';
+
+        boolean myBoolean = false;
+
+        char registered = '\u00AE';
+        System.out.println(registered);
+    }
+}
