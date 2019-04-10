09844aeb904948c1a4dc9b230e17da178775270b
Edward Leslie <edwardleslie09@gmail.com>
2019-04-10 15:00:44 -0400

    completed lesson 31 more methods

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -7,16 +7,46 @@ public class Main {
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
 
+//        int highScore = calculateScore(true, 800, 5, 100);
+//        System.out.println("Your final score was " + highScore);
+
+        int highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Test1", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Test2", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Test3", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Test4", highScorePosition);
+
+
     }
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
-    return -1;
+        return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        }
+        return 4;
     }
 }
