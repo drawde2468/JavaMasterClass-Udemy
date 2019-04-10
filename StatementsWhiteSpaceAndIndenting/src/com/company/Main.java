4b91b70e4ee672711d1ce5ae4ca7457a1bcfe01d
Edward Leslie <edwardleslie09@gmail.com>
2019-04-10 14:13:03 -0400

    completed through lesson 28

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50; //statement entire line
+        myVariable++; //statement
+        myVariable--; //statement
+
+        System.out.println("This is a test"); //statement
+
+        System.out.printf("This is" +
+                " another" +
+                " still " +
+                "more."); //statement even though multiple lines
+
+
+
+    }
+}
