e4b611542eff5ba14a15444a817840177e0b2c7e
Edward Leslie <edwardleslie09@gmail.com>
2019-04-10 13:22:01 -0400

    completed lesson 26 Keywords and Expressions

---
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //a mile is equal to 1.609344
+        double kilometers = (100* 1.609344); //kilometers = (100* 1.609344) is an expression
+
+        int highScore = 50; //highScore = 50 is an expression
+        if(highScore == 50){ //highScore == 50 is an expression
+            System.out.printf("This is an expression"); //"This is an expression" is an expression
+        }
+
+
+
+
+    }
+}
