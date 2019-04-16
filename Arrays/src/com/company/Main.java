993ee9f46f582b52c7e8ba169b8f17ff95edc013
Edward Leslie <edwardleslie09@gmail.com>
2019-04-16 11:58:06 -0400

    completed lesson 81 Arrays

---
+++ Arrays/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] myIntergers = getIntegers(5);
+
+        for (int i = 0; i < myIntergers.length; i++) {
+            System.out.println("Element " + i + ", typed value is " + myIntergers[i]);
+        }
+
+        System.out.println("The average is " + getAverage(myIntergers));
+
+
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values. \r");
+        int[] values = new int[number];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return (double) sum / (double) array.length;
+    }
+
+}
