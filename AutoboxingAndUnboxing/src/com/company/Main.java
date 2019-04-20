212220c98e1dcdd382982d01928fd560379f7695
Edward Leslie <edwardleslie09@gmail.com>
2019-04-19 22:45:52 -0400

    completed lesson 94 Autoboxing and Unboxing

---
+++ AutoboxingAndUnboxing/src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.ArrayList;
+
+class intClass {
+    private int myValue;
+
+    public intClass(int myValue) {
+        this.myValue = myValue;
+    }
+
+    public int getMyValue() {
+        return myValue;
+    }
+
+    public void setMyValue(int myValue) {
+        this.myValue = myValue;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] strArray = new String[10];
+        int[] intArray = new int[10];
+
+        ArrayList<String> strArrayList = new ArrayList<>();
+        strArrayList.add("Ed");
+
+//        ArrayList<int> intArrayList = new ArrayList<int>(); Cannot be primitive datatype
+        ArrayList<intClass> intClassArrayList = new ArrayList<>();
+        intClassArrayList.add(new intClass(54));
+
+        Integer integer = new Integer(54);
+        Double doubleValue = new Double(12.25);
+
+        ArrayList<Integer> intArrayList = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            intArrayList.add(Integer.valueOf(i));
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i + " --> " + intArrayList.get(i).intValue());
+        }
+
+        Integer myIntValue = 56;  //Integer.valueOf(56) Java is converting this code to the longer method detailed above
+
+        ArrayList<Double> myDoubleValues = new ArrayList<>();
+
+        for(double dbl = 0.0; dbl <= 10.0; dbl+= 0.5){
+            myDoubleValues.add(dbl);
+        }
+
+        for(int i = 0; i < myDoubleValues.size(); i++){
+            System.out.println( i + " --> " + myDoubleValues.get(i));
+        }
+
+
+    }
+}
